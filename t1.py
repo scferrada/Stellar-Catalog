f20ddda69c24195a2b8821113e4843eb06c941a8
Claudin <claudio.berroeta@ing.uchile.cl>
2014-09-24 17:18:06 -0300

    Signed-off-by: Claudin <claudio.berroeta@ing.uchile.cl>

--- t1.py
+++ t1.py
@@ -1,24 +1,36 @@
 __author__ = 'Sebastian'
 
+
 import metodos_t1
 import pyfits
 import numpy as np
 from flux20_lib import *
+from fits_lib import plot_image, get_fits_matrix, get_fits_header, print_header
 
 
 f1 = "out/blank.fits"
-f2 = "res/stellar1.dat"
-hdu = pyfits.open(f1)
-metodos_t1.addStellarCatalog(hdu,f2)
-hdu.close()
+f2 = "res/stellar.dat"
+f3 = "m83.fits"
+
+
+#hdu = pyfits.open(f1)
+#print type(hdu[0].data)
+#metodos_t1.addStellarCatalog(hdu,f2)
+#hdu.close()
 
 
 
-arch   = pyfits.open(f1)
-hdr    = arch[0].header
-img    = arch[0].data
+arch   = pyfits.open(f1,mode="update")
 
+metodos_t1.addStellarCatalog(arch,f2)
+arch.flush()
+arch.close()
+
+le = pyfits.open(f1) 
+#hdr    = arch[0].header
+img    = le[0].data
+print img[2532][878]
 print(img)
 
 from fits_lib import plot_image
-plot_image(img,log_scale=True,title=r'$\mathrm{\mathbb{Objetos\,del\,cat\acute{a}logo\,marcados\,con\,signo\,\plus}}$')
+plot_image(img,log_scale=False,title=r'$\mathrm{\mathbb{Objetos\,del\,cat\acute{a}logo\,marcados\,con\,signo\,\plus}}$')
